123e0940595d28696519b81d0058823257920f83
alice <aliceortega1009@gmail.com>
2026-04-07 11:41:09 -0300

    07/04

---
+++ mobile/WidgetEstrutura/intro_layout/lib/widgets/bnbwidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:intro_interacao/pages/contato_page.dart';
+import 'package:intro_interacao/pages/formulario_page.dart';
+import 'package:intro_interacao/pages/home_page.dart';
+
+int _pagesIndex = 0;
+
+final List<Widgets> _pages = [
+  HomePage(), FormularioPage(), ContatoPage()
+];
+
+class Bnbwidget {
+    
+    bottomNavigationBar ()
+}
